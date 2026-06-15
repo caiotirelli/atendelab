5a4487125ba2d8ea030fdf7a35bf0aa51e2d3b2f
caiotirelli <112010086+caiotirelli@users.noreply.github.com>
2026-06-14 21:43:49 -0300

    Add files via upload

---
+++ atendelab/public/teste-usuarios-post.php
@@ -0,0 +1,21 @@
+<?php
+// Arquivo temporário apenas para teste didático.
+
+$dados = [
+    'nome'   => 'Maria Teste',
+    'email'  => 'joao3@atendelab.com',
+    'senha'  => '123456',
+    'perfil' => 'atendente',
+    'status' => 'ativo'
+];
+
+$ch = curl_init('http://localhost/atendelab/public/?controller=usuarios&action=criar');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($dados));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$resposta = curl_exec($ch);
+
+echo '<pre>';
+echo $resposta;
+echo '</pre>';
